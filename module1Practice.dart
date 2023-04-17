3e1d9c9b1d7f9f9ed819935d7113c0ba6dee8996
Md. Firose Mahmud <105736440+firose-munna@users.noreply.github.com>
2023-04-17 21:59:30 +0600

    Update module1Practice.dart

--- module1Practice.dart
+++ module1Practice.dart
@@ -4,7 +4,8 @@ void main(){
 
   int a = 20;
   int b = 8;
-  String str = "My name is Munna";
+  String str = "My name is Munna.";
+  int age = 23;
   double height = 5.5;
   bool isBangladeshi = true;
 
@@ -25,6 +26,30 @@ void main(){
   print(mul);
   print(div);
   print(mod);
+
+  print(str +" Age is : " +age.toString() + " Years old");
+
+
+  List<int> numList = [1, 2, 45, 76, 87];
+  print("Number List is : "+numList.toString());
+  print("First Number of the List is : "+numList.first.toString());
+  print("Last Number of the list is : "+numList.last.toString());
+  print("Length of the list is "+numList.length.toString());
+  print(numList[3]);
+  print("The last number of this list is ${numList.last}");
+
+
+  Map<int, String> stdnt ={
+    1: "Firose",
+    2: "Munna",
+    3: "Munmun",
+    4: "Babu",
+    5: "Rafiqul"
+  };
+  print(stdnt);
+  print(stdnt[1]);
+  print(stdnt.length);
+ 
 
 
 
