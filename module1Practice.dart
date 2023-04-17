00f7187aff6b28f9c3a7ed50654f22a125f1a95d
Md. Firose Mahmud <105736440+firose-munna@users.noreply.github.com>
2023-04-17 21:27:16 +0600

    Add files via upload

---
+++ module1Practice.dart
@@ -0,0 +1,31 @@
+void main(){
+  print("Hello World");
+
+
+  int a = 20;
+  int b = 8;
+  String str = "My name is Munna";
+  double height = 5.5;
+  bool isBangladeshi = true;
+
+  print(a);
+  print(b);
+  print(str);
+  print(height);
+  print(isBangladeshi);
+
+  int sum = a +b;
+  int sub = a-b;
+  int mul = a*b;
+  double div = a/b;
+  int mod = a%b;
+
+  print(sum);
+  print(sub);
+  print(mul);
+  print(div);
+  print(mod);
+
+
+
+}
