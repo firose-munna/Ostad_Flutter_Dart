42257bc924a567604dc70ea14656b0c7755fd076
Md. Firose Mahmud <105736440+firose-munna@users.noreply.github.com>
2023-05-23 00:04:39 +0600

    Add files via upload

---
+++ LiveTest/liveTest4.dart
@@ -0,0 +1,22 @@
+class Media{
+  void play(){
+    print("Playing media...");
+  }
+}
+class Song extends Media{
+  String artist;
+  Song(this.artist);
+  @override
+  void play() {
+    // TODO: implement play
+    print("Playing song by $artist...");
+  }
+}
+
+void main(){
+  Media media = Media();
+  Song song = Song("Balam");
+
+  media.play();
+  song.play();
+}
