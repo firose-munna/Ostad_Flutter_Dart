82383cbc3041430d49cc8d5e9cdc8a7f6e4acf07
Md. Firose Mahmud <105736440+firose-munna@users.noreply.github.com>
2023-05-29 19:16:25 +0600

    Add files via upload

---
+++ Project/calculator.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+dynamic sum(var a, var b){
+  return a+b;
+}
+dynamic sub(var a, var b){
+  return a-b;
+}
+dynamic mult(var a, var b){
+  return a*b;
+}
+dynamic div(var a, var b){
+  return a/b;
+}
+dynamic mod(var a, var b){
+  return a%b;
+}
+dynamic pow(var a, var b){
+  var sum =a;
+  for(int i=1; i<b; i++){
+    sum*=a;
+  }
+  return sum;
+}
+
+void calculator(){
+  var result;
+  print("Enter Two Integer Numbers: ");
+  var n1 = int.parse(stdin.readLineSync()!);
+  var n2 = int.parse(stdin.readLineSync()!);
+
+  print("Enter the operator : ");
+  String op = stdin.readLineSync()!;
+
+  if(op == "+"){
+    result = sum(n1, n2);
+  }
+  else if(op == "-"){
+    result = sub(n1, n2);
+  }
+  else if(op == "*"){
+    result = mult(n1, n2);
+  }
+
+  else if(op == "/"){
+    result = div(n1, n2);
+  }
+  else if(op == "%"){
+    result = mod(n1, n2);
+  }
+  else if(op == "^"){
+    result = pow(n1, n2);
+  }
+  else{
+    print("Invalid Operator");
+  }
+
+  print("Result = $result");
+}
+void main(){
+  var t=0;
+  while(t==0){
+    calculator();
+    print("Calculate Again (y/n) : ");
+    String check = stdin.readLineSync()!;
+    if(check == 'y'){
+      t=0;
+    }
+    else if(check == 'n'){
+      t=1;
+    }
+    else{
+      print("Invalid Command");
+    }
+  }
+
+
+
+}
