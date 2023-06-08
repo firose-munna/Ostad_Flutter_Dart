4523b4ff70d70d4c44a81a8c0758801d041ca796
Md. Firose Mahmud <105736440+firose-munna@users.noreply.github.com>
2023-06-08 23:51:19 +0600

    Add files via upload

---
+++ LiveTest/liveTest1.dart
@@ -0,0 +1,11 @@
+void main(){
+  Map<String, dynamic> employee={
+    "Id" : 1,
+    "Name": "Firose Munna",
+    "Age" : 23,
+    "Salary" : 70000,
+    "Gender" : "Male"
+  };
+
+  print(employee["Salary"]);
+}
